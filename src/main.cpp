b39c2e8834dd43aea9eda1f55f305bc5d4691fa8
Riccardo Sartori <riccardo.sartori87@gmail.com>
2020-02-27 22:45:57 +0100

    Limit frame rate to 60 FPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,25 @@
+#include <SDL.h>
+
 #include "Window.h"
 #include "InputHandler.h"
 
+const int FPS = 60;
+const int DELAY_TIME = 1000.0f / FPS;
+
 int main()
 {
+  Uint32 frameStart, frameTime;
   Window window("Test Window", 100, 100, 640, 480);
   InputHandler inputHandler;
 
-  while (inputHandler.update()) {};
+  while (inputHandler.update()) {
+    frameStart = SDL_GetTicks();
+    frameTime = SDL_GetTicks() - frameStart;
+    if (frameTime < DELAY_TIME)
+      {
+	SDL_Delay((int) (DELAY_TIME - frameTime));
+      }
+  };
   
   return 0;
 }
